5a6c61a1bfa37194e268446d20bff2b526b00ef3
Baruch Velez <baruchvelez@uwotm8.local>
2016-07-05 02:32:26 -0400

    rename taskformsvc containing folder

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsComponent } from './../forms';
-import { TaskFormService } from './shared/index';
+import { TaskFormService } from './forms/index';
 
 @Component({
   moduleId: module.id,
