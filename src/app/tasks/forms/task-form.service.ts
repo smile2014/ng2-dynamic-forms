572a26eb0ccf0a7657b7a201858f6754779b8682
Baruch Velez <baruchvelez@uwotm8.local>
2016-07-06 00:57:10 -0400

    parent child share classes in form component, also cleaned up

--- src/app/tasks/forms/task-form.service.ts
+++ src/app/tasks/forms/task-form.service.ts
@@ -14,69 +14,19 @@ export class TaskFormService {
         type: 'text',
         order: 1
       }),
-      new FormTextbox({
-        key: 'bug-title',
-        label: 'Bug Title',
+      new FormDropdown({
+        key: 'dropdown',
+        label: 'Drop Down',
         required: false,
-        type: 'text',
-        order: 2
-      }),
-      new FormTextbox({
-        key: 'customer',
-        label: 'Customer',
-        required: false,
-        type: 'text',
-        order: 3
-      }),
-      new FormTextbox({
-        key: 'workflow',
-        label: 'Workflow Step',
-        required: false,
-        type: 'text',
-        order: 4
-      }),
-      new FormTextbox({
-        key: 'description',
-        label: 'Description',
-        required: false,
-        type: 'text',
-        order: 5
-      }),
-      new FormTextbox({
-        key: 'comment-text',
-        label: 'Last Comment',
-        required: false,
-        type: 'text',
-        order: 6
-      }),
-      new FormTextbox({
-        key: 'comment-author',
-        label: 'Last Comment Author',
-        required: false,
-        type: 'text',
-        order: 7
-      }),
-      new FormTextbox({
-        key: 'comment-date',
-        label: 'Last Comment Date',
-        required: false,
-        type: 'text',
-        order: 8
-      }),
-      new FormTextbox({
-        key: 'extra-text',
-        label: 'Extra Information',
-        required: false,
-        type: 'text',
-        order: 9
-      }),
-      new FormTextbox({
-        key: 'extra-author',
-        label: 'Extra Information Author',
-        required: false,
-        type: 'text',
-        order: 10
-      })
+        order: 2,
+        options: [ {
+          key: 'foo',
+          value: 'Foo'
+        },{
+          key: 'bar',
+          value: 'Bar'
+        }]
+,      })
     ];
 
     return questions.sort((a, b) => a.order - b.order);
